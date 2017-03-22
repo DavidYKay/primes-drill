b910f3a4550d8a115f119f79b4430516a8239c00
David Young-chan Kay <davidykay@gmail.com>
2017-03-22 09:12:10 -0700

    also works for primes below 100

--- test/primes/core_test.clj
+++ test/primes/core_test.clj
@@ -10,4 +10,9 @@
 
 (facts "Prime Sieve"
        (fact "Can find primes below 30"
-             (prime-sieve 30) => [2 3 5 7 11 13 17 19 23 29]))
+             (prime-sieve 30) => [2 3 5 7 11 13 17 19 23 29])
+
+       (fact "Can find primes below 100"
+             (prime-sieve 100) => [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97])
+       
+       )
